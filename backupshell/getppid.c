b89ffe16657a81c9715c45b85ef83d6822e1d8dd
F-O-P <tayler.coon@gmail.com>
2023-04-06 09:24:35 -0700

    everything you need to know

--- backupshell/getppid.c
+++ backupshell/getppid.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * getppid - returns the process id of the calling process
+ * @pid: the pid of the process
+ * Return: the process id of the calling process
  */
 
-int getppid(int pid)
+void getppid(int pid)
 {
-	
+	pid_t parent_pid = getppid();
+    printf("Parent process PID: %d\n", parent_pid);
 }
