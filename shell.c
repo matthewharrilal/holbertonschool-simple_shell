5647862a84f8573031cb7a250e356c1eb4bcfcf2
sarahmarkland <sarah.markland@icloud.com>
2023-04-17 13:20:55 -0700

    we did it

--- shell.c
+++ shell.c
@@ -1,55 +1,52 @@
 #include "shell.h"
 
-extern char **environ;
+void no_more_betty(void);
 
 /**
  * main - Entry point for ghost
  * @argc: arg count
  * @argv: array of input arg strings
+ * @env: array of env strings
  * Return: Always 0. Exit status differs.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv, char **env)
 {
-	char *comsave = NULL, *prompt = NULL, *path = NULL, *token = NULL;
-	char *line = NULL, *args[40], *paths[40], *pname = argv[0], *env = NULL;
+	char *line = NULL, *path = NULL;
 	size_t nread = 0;
-	int i = 0, j = 0;
-	char *comp_path = NULL;
-	struct stat x;
+	char *args[20], *paths[20];
+	int ret_val = 0;
+	int i;
 
-	prompt = "$ ";
+	(void) argc;
+	(void) argv;
 	while (1)
 	{
-		path = strdup(getenv("PATH"));
-		tokenize_path(path, paths);
+		i = 0;
+		while (env[i])
+		{
+			if (_strncmp(env[i], "PATH=", 5) == 0)
+			{
+				path = _strdup((env[i] + 5));
+				break;
+			}
+			i++;
+		}
+		tokenize_string(path, ":", paths);
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, prompt, _strlen(prompt));
-		if (getline(&line, &nread, stdin) < 0)
+			write(STDOUT_FILENO, "$aylerMoon: ", 12);
+		if (getline(&line, &nread, stdin) == -1)
 		{
 			free(line);
 			free(path);
 			exit(EXIT_SUCCESS);
 		}
-		cleanstr(line);
-		if (line[0] == '\0')
+		tokenize_string(line, " \n\t", args);
+
+		if (!args[0])
 		{
 			free(path);
 			continue;
 		}
-		token = tokstr(line, DELIMS, &comsave);
-		if (!token[0])
-		{
-			free(path);
-			continue;
-		}
-		 i = 0;
-		while (token != NULL)
-		{
-			args[i] = token;
-			i++;
-			token = tokstr(NULL, DELIMS, &comsave);
-		}
-		args[i] = NULL;
 		if (_strcmp(args[0], "exit") == 0)
 		{
 			free(line);
@@ -58,39 +55,141 @@ int main(int argc, char *argv[])
 		}
 		if (_strcmp(args[0], "env") == 0)
 		{
-			for (j = 0; environ[j]; j++)
+			for (i = 0 ; env[i] != NULL ; i++)
 			{
-				env = environ[j];
-				write(STDOUT_FILENO, env, _strlen(env));
+				write(STDOUT_FILENO, env[i], _strlen(env[i]));
 				write(STDOUT_FILENO, "\n", 1);
 			}
 			free(path);
 			continue;
 		}
+
 		if (access(args[0], X_OK) == 0)
-			make_a_baby(args[0], args);
+			make_a_baby(path, args[0], args);
 		else
-		{
-			i = 0;
-			while (paths[i] != NULL)
-			{
-				comp_path = malloc(_strlen(args[0]) + _strlen(paths[i]) + 2);
-				_strcpy(comp_path, paths[i]);
-				_strcat(comp_path, "/");
-				_strcat(comp_path, args[0]);
-				if (stat(comp_path, &x) == 0)
-				{
-					make_a_baby(comp_path, args);
-					free(comp_path);
-					break;
-				}
-				free(comp_path);
-				i++;
-			}
-		}
-		free(path);
+			ret_val = check_path(path, paths, args);
+
 	}
-	(void) argc;
-	(void) pname;
-	return (0);
+	return (ret_val);
+}
+/**
+ * tokenize_string - tokenize a string
+ * @line: string to tokenize
+ * @delims: delimiter string
+ * @tokens: array to save tokens
+ * Return: void
+ */
+void tokenize_string(char *line, char *delims, char **tokens)
+{
+	char *path_token = strtok(line, delims);
+	int i = 0;
+
+	while (path_token != NULL)
+	{
+		tokens[i] = path_token;
+		i++;
+		path_token = strtok(NULL, delims);
+	}
+	tokens[i] = NULL;
+}
+/**
+* check_path - checks if command is in path
+* @path: path string
+* @patharr: array of path strings
+* @args: array of input arg strings
+* Return: 0 on success, 1 on failure
+*/
+int check_path(char *path, char **patharr, char **args)
+{
+	int i = 0;
+	char *comp_path = NULL;
+	struct stat x;
+
+	while (patharr[i] != NULL)
+	{
+		comp_path = malloc(_strlen(args[0]) + _strlen(patharr[i]) + 2);
+		_strcpy(comp_path, patharr[i]);
+		_strcat(comp_path, "/");
+		_strcat(comp_path, args[0]);
+		if (stat(comp_path, &x) == 0)
+		{
+			make_a_baby(path, comp_path, args);
+			free(comp_path);
+			return (0);
+		}
+		free(comp_path);
+		i++;
+	}
+	return (127);
+}
+
+
+/**
+ * make_a_baby - creates a child process
+ * @stdpath: path string
+ * @call_path: path to executable
+ * @str_arr: array of tokens
+ * Return: status of child process
+ */
+
+int make_a_baby(char *stdpath, char *call_path, char **str_arr)
+{
+	pid_t cop;
+	pid_t sig;
+	int status = 0;
+
+	free(stdpath);
+	cop = fork();
+	if (cop == 0)
+	{
+		if (execve(call_path, str_arr, NULL) == -1)
+			exit(EXIT_FAILURE);
+	}
+	else if (cop < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		do {
+			sig = waitpid(cop, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	(void) sig;
+	return (status);
+}
+/**
+ * *_strdup - ret ptr to newly allocated mem space
+ * @str: string
+ * Return: NULL or ptr to dupe string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int n = 0;
+	char *ar;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+		n++;
+	}
+	n++;
+
+	ar = malloc(n * sizeof(char));
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		ar[i] = str[i];
+	}
+
+	return (ar);
 }
--- shell.h
+++ shell.h
@@ -1,35 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <string.h>
 #include <sys/stat.h>
 
-#define LINE_CAP 1024
-#define DELIMS " \t\n\a"
+/* Prototypes */
+/*char **split_string(char *str);*/
+/*char **split_path(char *thePath);*/
+void tokenize_string(char *str, char *delims, char **tokens);
+int make_a_baby(char *stdpath, char *call_path, char **str_arr);
+int check_path(char *stdpath, char **path_array, char **token_array);
 
-void sig_stop(int sNum);
-char *tokstr(char *str, const char *delim, char **saveptr);
-unsigned int _strspn(char *s, const char *accept);
-char *_strchr(const char *s, char c);
-char *_strpbrk(char *s, const char *accept);
-int cleanstr(char *line);
+/*String Helpers*/
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
 int _strcmp(char *s1, char *s2);
-void tokenize_path(char *path, char **paths);
-int make_a_baby(char *comp_path, char **tokens);
-int path_finder(char **tokens);
-void tokenize_path_the_sequel(char *line, char *delims, char **tokens);
-void path_sweeper(char *path);
+char *_strdup(char *str);
 
 #endif
